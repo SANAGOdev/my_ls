eea798244353db3a42146ca38d289e143f3cfe7b
sanago <sanago3130@gmail.com>
2022-11-19 17:16:12 +0100

    FIX: -l flag

--- src/count_content.c
+++ src/count_content.c
@@ -6,32 +6,24 @@
 */
 
 #include "../include/my.h"
-int count_loop(struct dirent *entity, int count);
 
 int count_content(struct dirent *entity)
 {
     int count = 0;
     if (entity->d_type == 8)
         count = 1;
-    if (entity->d_type == 4) {
+    else if (entity->d_type == 4) {
         count = 2;
-        count_loop(entity, count);
+        DIR *dir = opendir(entity->d_name);
+        struct dirent *sub_entity = readdir(dir);
+        while (sub_entity != NULL) {
+            if (sub_entity->d_name[0] == '.')
+                NULL;
+            else if (sub_entity->d_type == 4) {
+                count += 1;
+            }
+            sub_entity = readdir(dir);
+        }
     }
     return count;
-}
-
-int count_loop(struct dirent *entity, int count)
-{
-    DIR *dir = opendir(entity->d_name);
-    struct dirent *sub_entity = readdir(dir);
-    while (sub_entity != NULL) {
-        if (sub_entity->d_name[0] == '.')
-            NULL;
-    }
-    while (sub_entity != NULL) {
-        if (sub_entity->d_type == 4)
-            count += 1;
-        sub_entity = readdir(dir);
-
-    }
 }
